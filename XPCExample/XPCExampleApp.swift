9bcbf5b30b63a92f4b8cafdbc5711b667e045e87
Cole Roberts <cole.alan.roberts@gmail.com>
2022-09-03 19:46:29 -0500

    Initial Commit

---
+++ XPCExample/XPCExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  XPCExampleApp.swift
+//  XPCExample
+//
+//  Created by Cole Roberts on 9/3/22.
+//
+
+import SwiftUI
+
+@main
+struct XPCExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
